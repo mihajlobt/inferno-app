4793102640f18bfeb7f1f34f8e36ad1b9f4775b7
mihajlobt <mihajlobt@gmail.com>
2017-04-30 00:45:47 +0200

    inferno

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import { version } from 'inferno';
+import Component from 'inferno-component';
+import Logo from './logo';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <Logo width="80" height="80" />
+          <h2>{`Welcome to Inferno ${version}`}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
